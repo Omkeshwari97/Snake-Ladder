ebcf32e2191d4cef1796e624491e4df21b1cab66
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-09-28 00:53:23 +0530

    UC4 Commit

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -8,25 +8,37 @@ public class SnakeLadder
 
 	public static void main(String args[])
 	{
-		int position=0;
-		//System.out.println(Math.random()*5);
-        int dieNumber=1 + (int)(Math.random() * ((6 - 1) + 1));
-        System.out.println("Number when die rolled: "+dieNumber);
+		int position=0,dieNumber=0;
 
-		int options=1 + (int)(Math.random() * ((3 - 1) + 1));
+		do
+		{	
+			if(position<0)
+				position=0;
+				
+			//System.out.println(Math.random()*5);
+			dieNumber=1 + (int)(Math.random() * ((6 - 1) + 1));
+			System.out.println("Number when die rolled: "+dieNumber);
 
-		switch (options) 
-		{
-			case NO_PLAY:	position=position+0;
-							break;
-			case LADDER:	position=position+dieNumber;
-							break;
-			case SNAKE:		position=position-dieNumber;
-							break;
-			default:		break;
-		}
+			int options=1 + (int)(Math.random() * ((3 - 1) + 1));
 
-		System.out.println("Options: "+options);
-		System.out.println("Current Die Postion: "+position);
+			switch (options) 
+			{
+				case NO_PLAY:	System.out.println("NO PLAY");
+								position=position+0;
+								break;
+				case LADDER:	System.out.println("LADDER");
+								position=position+dieNumber;
+								break;
+				case SNAKE:		System.out.println("SNAKE");
+								position=position-dieNumber;
+								break;
+				default:		break;
+			}
+
+			System.out.println("Current Die Postion: "+position);
+		}while(position<=100);
+		
+		System.out.println("Final Position: "+position);
 	}
+
 }
